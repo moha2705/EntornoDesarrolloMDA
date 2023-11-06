16a7510f2b1a9d6e1046b7fd4b323fd5518593a2
mdahmania01 <mdahmania01@educarex.es>
2023-11-06 14:22:40 +0100

    Bnoches Proyecto

--- src/Miproyecto/ProyectoA.java
+++ src/Miproyecto/ProyectoA.java
@@ -4,7 +4,8 @@ public class ProyectoA {
 
 	public static void main(String[] args) {
 System.out.println("Hola mundo");
-	System.out.println("Adios Mundo");
+	System.out.println("Buenas tardes Mundo");
+	System.out.println("Buenas noches mundo");
 	}
 
 }
