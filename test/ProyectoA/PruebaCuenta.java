8430fbcbccc8bd001410845edd137405d8695611
diurno <diurno@equipo-00>
2024-02-16 14:22:55 +0100

    prueba

---
+++ test/ProyectoA/PruebaCuenta.java
@@ -0,0 +1,14 @@
+package ProyectoA;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PruebaCuenta {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
