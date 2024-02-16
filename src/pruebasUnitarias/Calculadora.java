92f0d7c7064a1995c16efe8b9625a8d24a652c94
diurno <diurno@equipo-00>
2024-02-16 14:21:56 +0100

    ejerccio prueba cuenta

--- src/pruebasUnitarias/Calculadora.java
+++ src/pruebasUnitarias/Calculadora.java
@@ -3,16 +3,23 @@ package pruebasUnitarias;
 public class Calculadora {
 
 	public static void main(String[] args) {
-		int num1=3 , num2=2;
-		int resul=suma(num1,num2);
-		System.out.println("El resultado de sumar 3+2="+resul);
+		int num1 = 3, num2 = 2;
+		int resul = suma(num1, num2);
+		System.out.println("El resultado de sumar 3+2=" + resul);
 
 	}
-	public static int resta(int a,int b) {
-		return a-b;
+
+	public static int resta(int a, int b) {
+		return a - b;
 	}
 
-	public static int suma(int a,int b) {
-		return a+b;
+	public static int suma(int a, int b) {
+		return a + b;
+	}
+
+	public static int factorial(int num) {
+		
+		if(num==0||num==1)return 1;
+		return num * factorial(num - 1);
 	}
 }
--- test/ProyectoA/CalculadorTest.java
+++ test/ProyectoA/CalculadorTest.java
@@ -14,6 +14,17 @@ public class CalculadorTest {
 		int esperado=7;
 		assertEquals(esperado, resultado);
 	}
-	
+	@Test
+	public void testResta() {
+		int resultado=Calculadora.resta(4, 3);
+		int esperado=1;
+		assertEquals(esperado, resultado);
+	}
+	@Test
+	public void testFactorial() {
+		int resultado=Calculadora.factorial(5);
+		int esperado=120;
+		assertEquals( esperado,resultado);
+	}
 
 }
